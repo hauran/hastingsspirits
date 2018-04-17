9416618901dd2b624dc8d44927eee1895cfa185d
rmai <richardmai@gmail.com>
2018-04-17 13:48:29 -0400

    done

--- src/App.js
+++ src/App.js
@@ -5,21 +5,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hastings Spirits</h1>
-        <p>
-          <div>Waterfront Beer Garden</div>
-          <div>Hastings on Hudson, NY</div>
-          <div>Spring 2019</div>
-        </p>
         <p>
           <div className='logo'>
             <img src='HS2.png' alt='logo'/>
           </div>
         </p>
+        <p>
+          <div style={{fontSize:'1.5em', fontWeight:'bold'}}>Waterfront Beer Garden</div>
+          <div style={{color:'#666', marginTop:20}}>Hastings on Hudson, NY</div>
+          <div style={{fontSize:'0.9em',color:'#666'}}>Spring 2019</div>
+        </p>
       </div>
-      
     );
   }
 }
-
 export default App;
